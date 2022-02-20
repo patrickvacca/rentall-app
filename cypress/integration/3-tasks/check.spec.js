8d0e099811e219ec2ac7dbcc4ef8eea4ca6fbf2a
patrick.vacca <patrick.vacca@mbp.local>
2022-02-19 21:35:11 -0500

    updated cypress tests

--- cypress/integration/3-tasks/check.spec.js
+++ cypress/integration/3-tasks/check.spec.js
@@ -2,7 +2,8 @@ describe('Check a Task', () => {
     it('Visit Rentall Challenge App', () => {
         cy.visit('http://localhost:3000/')
 
-        cy.get(':nth-child(1) > .MuiListItem-root > .css-cvhtoe-MuiButtonBase-root-MuiListItemButton-root > .MuiListItemIcon-root > .MuiCheckbox-root > .PrivateSwitchBase-input').click()
+        cy.get('.MuiListItemIcon-root.css-cveggr-MuiListItemIcon-root').eq(0).click()
+        cy.get('#app-snackbar').contains('Task successfully updated!')
         cy.get('.MuiListItemText-root.css-tlelie-MuiListItemText-root').eq(0).should('have.css', 'text-decoration', 'line-through solid rgba(0, 0, 0, 0.87)')
     })
 });
@@ -12,7 +13,7 @@ describe('Uncheck a Task', () => {
         cy.visit('http://localhost:3000/')
 
         cy.get('.MuiListItemIcon-root.css-cveggr-MuiListItemIcon-root').eq(0).click()
-        cy.get('.MuiListItemIcon-root.css-cveggr-MuiListItemIcon-root').eq(0).click()
+        cy.get('#app-snackbar').contains('Task successfully updated!')
         cy.get('.MuiListItemText-root.css-tlelie-MuiListItemText-root').eq(0).should('have.css', 'text-decoration', 'none solid rgba(0, 0, 0, 0.87)')
     })
 });
